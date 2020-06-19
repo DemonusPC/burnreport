f206b66fdbd9e89b81c80d81933846c89021c4a5
Demonus <pogon.ap@hotmail.com>
2020-06-19 23:24:18 +0100

    Refactored the portion page into a single function table and form

--- frontend/src/pages/Portions/index.tsx
+++ frontend/src/pages/Portions/index.tsx
@@ -1,66 +1,56 @@
 import React, { useState, useEffect } from "react";
-import {
-  Heading,
-  Box,
-  Button,
-} from "grommet";
+import { Heading, Box, Button, Anchor } from "grommet";
+import { AddCircle } from "grommet-icons";
 
 import { useParams } from "react-router";
 
 import { Portion } from "../../util/schema/product";
 import PortionForm from "../../containers/PortionForm";
-import { Redirect } from "react-router-dom";
-import { postPortions, getProductSizesById, deletePortion } from "../../util/data/requests";
+import cogoToast from "cogo-toast";
+
+import {
+  postPortions,
+  getProductSizesById,
+  deletePortion,
+} from "../../util/data/requests";
 import PortionTable from "../../components/PortionTable";
 
 const emptyState = (): Array<Portion> => {
   return [];
 };
 
-const addPortion = (
+const removeAndDeletePortion = (
   state: Array<Portion>,
-  portion: Portion
-): Array<Portion> => {
-  if (state.some((c) => c.name === portion.name)) {
-    return state;
-  }
-
-  return state.concat([portion]);
-};
-
-
-
-const removePortion = (state: Array<Portion>, name: string) => {
-  const result = state.filter((p) => p.name !== name);
-  return result;
-};
-
-const removeAndDeletePortion = (state: Array<Portion>, name: string, productId: number, removeFunction: any) => {
+  name: string,
+  productId: number,
+  removeFunction: any
+) => {
   deletePortion(productId, name);
   const result = state.filter((p) => p.name !== name);
   return result;
-  
-}
+};
 
-const submit = (portions: Array<any>, setSent: any) => {
-  const parsed : Array<Portion> = portions.map((portion: any) => {
+const submit = async (portions: Array<any>) => {
+  const parsed: Array<Portion> = portions.map((portion: any) => {
     return {
       product: parseInt(portion.product),
       name: portion.name,
-      grams: parseFloat(portion.grams)
-    }
-  })
-
-  postPortions(parsed).then((result: { status: any; }) => {
-    setSent(true);
+      grams: parseFloat(portion.grams),
+    };
   });
-}
 
+  const result = await postPortions(parsed);
+  return result;
+};
+
+const refreshPortions = async (id: number, setState: any) => {
+  const portions = await getProductSizesById(id);
+  setState(portions);
+};
 
 const Portions = () => {
-  const [state, setState] = useState(emptyState());
   const [current, setCurrent] = useState(emptyState());
-  const [sent, setSent] = useState(false);
+  const [adding, setAdding] = useState(false);
   const { id } = useParams();
 
   useEffect(() => {
@@ -68,34 +58,55 @@ const Portions = () => {
       const portions = await getProductSizesById(id);
 
       setCurrent(portions);
-    }
+    };
 
     fetchAndSet();
   }, [id]);
 
   return (
-    <Box pad="large">
-      <Box width="50%" margin={{bottom: "large"}}>
-        <Heading>Current Portions</Heading>
-        <PortionTable portions={current} stateSetter={setCurrent} productId={id} stateReducer={removeAndDeletePortion} />
-      </Box>
-      <Box width="50%">
-        <Heading>Add Portion Sizes</Heading>
-        <PortionTable portions={state} stateSetter={setState} productId={id} stateReducer={removePortion} />
-
-        <PortionForm
-          product={id}
-          selectedFunction={(portion: Portion) =>
-            setState(addPortion(state, portion))
-          }
+    <>
+      <Box pad="large" margin="large" elevation="medium">
+        <Heading>Portions</Heading>
+        <PortionTable
+          portions={current}
+          stateSetter={setCurrent}
+          productId={id}
+          stateReducer={removeAndDeletePortion}
         />
+        <Box margin={{ top: "large" }}>
+          {!adding ? (
+            <Button
+              primary
+              alignSelf="start"
+              icon={<AddCircle />}
+              label="Add portion"
+              onClick={() => setAdding(true)}
+            />
+          ) : (
+            <>
+              <PortionForm
+                product={id}
+                selectedFunction={async (portion: Portion) => {
+                  const submitResult = await submit([portion]);
+                  if (submitResult) {
+                    cogoToast.success("Portion added");
+                    await refreshPortions(id, setCurrent);
+                  } else {
+                    cogoToast.error("Failed to add portion");
+                  }
+                  setAdding(false);
+                }}
+              />
+              <Anchor
+                margin={{ top: "medium" }}
+                label="Cancel"
+                onClick={() => setAdding(false)}
+              />
+            </>
+          )}
+        </Box>
       </Box>
-      <Box direction="row" gap="medium">
-      <Button type="submit" label="Submit" onClick={() => submit(state, setSent) } />
-      <Button type="reset" label="Clear" onClick={() => setState(emptyState())} />
-      </Box>
-      {sent && <Redirect to="/products" />}
-    </Box>
+    </>
   );
 };
 
