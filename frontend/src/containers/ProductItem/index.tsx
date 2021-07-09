56a9d767367dd69466d05eb2924d932596344025
Demonus <pogon.ap@hotmail.com>
2021-07-09 10:37:53 +0100

    Rewriting to use MaskedInput isntead of a TextField

--- frontend/src/containers/ProductItem/index.tsx
+++ frontend/src/containers/ProductItem/index.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import {
-  Button,
-  Box,
-  TextInput,
-  Text,
-  Select,
-} from "grommet";
+import { Button, Box, Text, Select, MaskedInput } from "grommet";
 import { Close } from "grommet-icons";
 import { ProductSize } from "../../util/schema/product";
 
@@ -14,13 +8,13 @@ const PerWrapper = styled(Box)`
   align-items: center;
 `;
 
-const shortName = (name: string) : string => {
-  const max = 28
-  if(name.length > max) {
+const shortName = (name: string): string => {
+  const max = 28;
+  if (name.length > max) {
     return name.slice(0, max - 3) + "...";
   }
   return name;
-}
+};
 
 interface ProductItemProps {
   id: number;
@@ -41,40 +35,54 @@ const ProductItem = ({
   deleteFunc,
   unit,
   unitOptions,
-  setUnit
+  setUnit,
 }: ProductItemProps) => {
-
   return (
-    <Box elevation="medium" pad="medium" margin={{bottom: "large"}} border="all" key={id}>
-      <Box  direction="row" alignContent="center" justify="between">
+    <Box
+      elevation="medium"
+      pad="medium"
+      margin={{ bottom: "large" }}
+      border="all"
+      key={id}
+    >
+      <Box direction="row" alignContent="center" justify="between">
         <Text weight="bold">{shortName(name)}</Text>
-        <Button
-          plain={true}
-          icon={<Close />}
-          onClick={deleteFunc}
+        <Button plain={true} icon={<Close />} onClick={deleteFunc} />
+      </Box>
+      <PerWrapper
+        direction="row"
+        alignContent="center"
+        justify="center"
+        margin={{ top: "medium" }}
+      >
+        <MaskedInput
+          mask={[
+            {
+              regexp: /^\d+$/,
+              placeholder: "0",
+            },
+            { fixed: "." },
+            {
+              length: 2,
+              regexp: /^[0-9]{1,4}$/,
+              placeholder: "00",
+            },
+          ]}
+          defaultValue={100.00}
+          onChange={changeFunc}
         />
-        </Box>
 
-          <PerWrapper
-            direction="row"
-            alignContent="center"
-            justify="center"
-            margin={{top: "medium"}}
-          >
-            <TextInput placeholder="0.0" value={amount} onChange={changeFunc} />
-
-            <Select
-              name="select"
-              placeholder="Select"
-              labelKey="name"
-              value={unit}
-              options={unitOptions}
-              onChange={({ option }) => {
-                setUnit(option);
-              }}
-            />
-          </PerWrapper>
-
+        <Select
+          name="select"
+          placeholder="Select"
+          labelKey="name"
+          value={unit}
+          options={unitOptions}
+          onChange={({ option }) => {
+            setUnit(option);
+          }}
+        />
+      </PerWrapper>
     </Box>
   );
 };
